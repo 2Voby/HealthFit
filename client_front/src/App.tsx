3d61166a042241c9a1bead32b4cc8248ca994ec3
User <gerasimcukvlad37@gmail.com>
2026-03-14 19:05:15 +0200

    add page layout

--- client_front/src/App.tsx
+++ client_front/src/App.tsx
@@ -3,15 +3,21 @@ import { Button } from "@/components/ui/button";
 import MainPage from "@/pages/MainPage/MainPage";
 import QuizPage from "@/pages/QuizPage/QuizPage";
 import ResultPage from "@/pages/ResultPage/ResultPage";
+import WithHeader from "@/layouts/WithHeader";
+import Blank from "@/layouts/Blank";
 
 export default function App() {
-	return (
-		<BrowserRouter>
-			<Routes>
-				<Route path="/" element={<MainPage />} />
-				<Route path="/quiz" element={<QuizPage />} />
-				<Route path="/result" element={<ResultPage />} />
-			</Routes>
-		</BrowserRouter>
-	);
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<WithHeader />}>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/result" element={<ResultPage />} />
+        </Route>
+        <Route element={<Blank />}>
+          <Route path="/quiz" element={<QuizPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
