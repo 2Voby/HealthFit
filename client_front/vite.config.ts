c1a7bca934c7b347f8a172d97286b0d22c9d0af0
Kyrylo <danilin2508@gmail.com>
2026-03-14 16:45:11 +0200

    add ngrok

--- client_front/vite.config.ts
+++ client_front/vite.config.ts
@@ -5,6 +5,9 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: ['nonparasitically-unrestored-luz.ngrok-free.dev'],
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
